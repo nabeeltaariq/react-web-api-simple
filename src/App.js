6334ff984c8590fe9680067c114a42d669f4e110
nabeeltaariq <nabeeltaariq@gmail.com>
2021-03-14 03:37:56 -0700

    first and final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from 'react'
+import './App.css'
+import useWebAnimations, { bounce } from '@wellyshen/use-web-animations'
 
 function App() {
+  const { ref, playState, getAnimation } = useWebAnimations({
+    ...bounce,
+  })
+  setInterval(() => {
+    getAnimation().play()
+  }, 1000)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <p>🍿 Animation is {playState}</p>
+      <div className='target' ref={ref}></div>
+      Current State of Animation : {playState}
+      <button onClick={() => getAnimation().pause()}>Stop</button>
+      <button onClick={() => getAnimation().play()}>Play</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
